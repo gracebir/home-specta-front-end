46870b56e7af22c4dc20f33e371e6082fa3889b1
gracebir <grace.devolop@gmail.com>
2024-07-03 21:29:54 +0200

    banner section

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,30 @@ export const navItems = [
     { id: 1, text: "TV Shows", href: "/" },
     { id: 2, text: "Discover", href: "/discover" },
 ];
+
+export const bannerItems = [
+    {
+        id: 1,
+        title: "Fleabag",
+        description:
+            "Series adapted from the award-winning play about a young woman trying to cope with life in London whilst coming to terms with a recent tragedy.",
+        backgroundUrl:
+            "https://res.cloudinary.com/dnzidlufh/image/upload/v1720024869/banner-1_dwvmec.png",
+    },
+    {
+        id: 2,
+        title: "Girls",
+        description:
+            "A comedy about the experiences of a group of girls in their early 20s",
+        backgroundUrl:
+            "https://res.cloudinary.com/dnzidlufh/image/upload/v1720024869/banner-2_a7igyr.png",
+    },
+    {
+        id: 1,
+        title: "Killing Eve",
+        description:
+            "After a series of events, the lives of a security operative and an assassin become inextricably linked.",
+        backgroundUrl:
+            "https://res.cloudinary.com/dnzidlufh/image/upload/v1720024868/banner-3_vvvlib.png",
+    },
+];
